7fd735c2758749427b375766243f4eb818c70d97
Alex C <cxz980314@gmail.com>
2022-02-10 18:13:14 -0500

    remove credits section

--- assets/generateREADME.js
+++ assets/generateREADME.js
@@ -19,28 +19,19 @@ ${data.description}
 
 - [Installation](#installation)
 - [Usage](#usage)
-- [Credits](#credits)
+- [How to Contribute](#contribution)
+- [Tests](#test)
+- [Questions](#email)
 - [License](#license)
 
 
 ## Installation
 
 ${data.installation}
-What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
 
 
 ## Usage
 ${data.usage}
-
-
-## Credits
-
-${data.contribution}
-List your collaborators, if any, with links to their GitHub profiles.
-
-If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
-
-If you followed tutorials, include links to those here as well.
 
 
 ## How to Contribute
